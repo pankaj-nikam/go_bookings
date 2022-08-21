4382c5c6cf90e661de6452104709e13a4aa03c3f
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2022-08-21 11:06:37 +0530

    Partially completed server side validation.

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -18,10 +18,17 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Has checks if form field is in post and is not empty.
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
+		f.Errors.Add(field, "This field cannot be blank.")
 		return false
 	}
 	return true
+}
+
+// Valid returns true if there are no errors, otherwise false.
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
 }
