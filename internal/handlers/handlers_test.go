5b3a4adea0ffb21060a0acd798da9eaa39e7778a
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2022-08-31 09:05:03 +0530

    Added post tests.

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -24,7 +25,23 @@ var theTests = []struct {
 	{"ms", "/majors-suite", "GET", []postData{}, http.StatusOK},
 	{"sa", "/search-availability", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
-	{"mr", "/make-reservation", "GET", []postData{}, http.StatusOK},
+
+	{"post-search-availability", "/search-availability", "POST", []postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-01"},
+	}, http.StatusOK},
+
+	{"post-search-availability-json", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2020-01-01"},
+		{key: "end", value: "2020-01-01"},
+	}, http.StatusOK},
+
+	{"make-reservation-post", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "Pankaj"},
+		{key: "last_name", value: "Nikam"},
+		{key: "email", value: "demo@demo.com"},
+		{key: "phone", value: "555-5555"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
@@ -40,6 +57,19 @@ func TestHandlers(t *testing.T) {
 			} else {
 				if response.StatusCode != test.expectedStatusCode {
 					t.Errorf("expected %d and got %d", test.expectedStatusCode, response.StatusCode)
+				}
+			}
+		} else if test.method == "POST" {
+			values := url.Values{}
+			for _, x := range test.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL+test.url, values)
+			if err != nil {
+				t.Error("error occurred")
+			} else {
+				if resp.StatusCode != test.expectedStatusCode {
+					t.Errorf("expected %d and got %d", test.expectedStatusCode, resp.StatusCode)
 				}
 			}
 		}
